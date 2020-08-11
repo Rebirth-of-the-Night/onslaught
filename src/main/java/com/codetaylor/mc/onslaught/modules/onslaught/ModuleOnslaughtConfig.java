9d70833fa57261fcb56526d8b684c0587d5254a8
codetaylor <jason@codetaylor.com>
2020-08-11 09:03:14 -0700

    Setup bare-bones config

---
+++ src/main/java/com/codetaylor/mc/onslaught/modules/onslaught/ModuleOnslaughtConfig.java
@@ -0,0 +1,8 @@
+package com.codetaylor.mc.onslaught.modules.onslaught;
+
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = ModuleOnslaught.MOD_ID, name = ModuleOnslaught.MOD_ID + "/" + "onslaught")
+public class ModuleOnslaughtConfig {
+
+}
