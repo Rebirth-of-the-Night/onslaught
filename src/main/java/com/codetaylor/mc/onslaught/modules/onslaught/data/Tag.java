1ef19bcd13f04ce1291c023487f5b5e388d6a2d2
codetaylor <jason@codetaylor.com>
2020-08-17 13:29:22 -0700

    Rename mining tag

--- src/main/java/com/codetaylor/mc/onslaught/modules/onslaught/data/Tag.java
+++ src/main/java/com/codetaylor/mc/onslaught/modules/onslaught/data/Tag.java
@@ -5,7 +5,7 @@ public final class Tag {
   public static final String ONSLAUGHT = "Onslaught";
   public static final String EXTRA_LOOT_TABLES = "ExtraLootTables";
   public static final String CUSTOM_AI = "CustomAI";
-  public static final String AI_MINING = "ai_mining";
+  public static final String AI_MINING = "Mining";
 
   private Tag() {
     //
